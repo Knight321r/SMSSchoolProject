b1497b5dd3c41ae022ac9b4589a60dcbb4cf91ab
Rohit <rohitnalapur@gmail.com>
2024-09-28 16:55:53 +0530

    registor type added

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,15 +5,21 @@ import smslogo from '../images/smslogo.jpeg'
 function Register() {
 
     const [username, setusername] = useState("")
+    const [type, settype] = useState("")
     const [password, setpassword] = useState("")
 
     const handleusername = (e) => {
         setusername(e.target.value)
     }
 
+    const handletype = (e) => {
+        settype(e.target.value)
+    }
+
     const handlepassword = (e) => {
         setpassword(e.target.value)
     }
+
 
     const handlereg = () => {
         //do the register stuff
@@ -30,6 +36,12 @@ function Register() {
         <h2>Sign in to your account</h2>
         <label for = "username">Email</label>
         <input id = "username" type = "text" onChange={(e) => handleusername(e)} ></input>
+        <label for = "type">Type</label>
+        <select id="type" value={type} onChange={(e) => handletype(e)}>
+            <option value="">Select type</option>
+            <option value="teacher">Teacher</option>
+            <option value="parent">Parent</option>
+        </select>
         <label for = "password">Password</label>
         <input id = "password" type = "password" onChange={(e) => handlepassword(e)} ></input>
         <button onClick={handlereg}>Register</button>
